ccbbd83b2ab2a6c31e572d33c07bf767243a4789
Erick Pac <erickj.pac@gmail.com>
2020-03-22 09:56:18 -0600

    [added]: some extensions

---
+++ gt-first/Source/Helpers/Extensions/UIFont+CustomFont.swift
@@ -0,0 +1,46 @@
+//
+//  UIFont+CustomFont.swift
+//  gt-first
+//
+//  Created by Erick Pac on 3/22/20.
+//  Copyright © 2020 Erick Pac. All rights reserved.
+//
+
+import UIKit
+
+enum FontWeight: String {
+    
+    case bold
+    case regular
+    case light
+    case thin
+    case semibold
+    
+    var name: String {
+        self.rawValue.capitalized
+    }
+    
+    func native() -> UIFont.Weight {
+        switch self {
+        case .bold:
+            return UIFont.Weight.bold
+        case .regular:
+            return UIFont.Weight.regular
+        case .light:
+            return UIFont.Weight.light
+        case .thin:
+            return UIFont.Weight.thin
+        case .semibold:
+            return UIFont.Weight.semibold
+        }
+    }
+    
+}
+
+extension UIFont {
+    
+    static func `default`(size: CGFloat, weight: FontWeight) -> UIFont {
+        return UIFont(name: "Raleway-\(weight.name)", size: size)!
+    }
+    
+}
