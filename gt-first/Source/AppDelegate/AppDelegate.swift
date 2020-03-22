b31ff9a42ab2495ab4ec18d5153e656d076993ba
Jose Quintero <>
2020-03-22 14:46:33 -0600

    Moved first launch validation to different screen

--- gt-first/Source/AppDelegate/AppDelegate.swift
+++ gt-first/Source/AppDelegate/AppDelegate.swift
@@ -12,19 +12,5 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
-    var isFirstLaunch: Bool {
-        return Storage.Key.launched.valueIs(value: false);
-    }
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow()
-        let board = isFirstLaunch
-            ? K.Storyboards.Onboarding.value
-            : K.Storyboards.Main.value;
-        window.rootViewController = board.instantiateInitialViewController()
-        window.makeKeyAndVisible()
-        return true
-    }
 
 }
--- gt-first/Source/Scenes/Main/ViewController.swift
+++ gt-first/Source/Scenes/Main/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isFirstLaunch: Bool {
+        return Storage.Key.launched.valueIs(value: false);
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestConfig(then: presentNextScreen())
     }
-
+    
+    func presentNextScreen() {
+        let board = isFirstLaunch
+            ? K.Storyboards.Onboarding.value
+            : K.Storyboards.Main.value
+        guard let controller = board.instantiateInitialViewController() else {
+            fatalError("Could not locate Storyboard controller")
+        }
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func requestConfig(then next: @autoclosure () -> Void) {
+        next()
+    }
 }
 
