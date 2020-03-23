71496db169ac89e03d2b27818a61c4dd2802ff6c
Jose Quintero <>
2020-03-22 23:30:14 -0600

    Initial login ui work

---
+++ gt-first/Source/Controls/Button.swift
@@ -0,0 +1,21 @@
+//
+//  Button.swift
+//  gt-first
+//
+//  Created by Jose Quintero on 3/22/20.
+//  Copyright © 2020 Erick Pac. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ gt-first/Source/Controls/TextField.swift
@@ -0,0 +1,21 @@
+//
+//  TextField.swift
+//  gt-first
+//
+//  Created by Jose Quintero on 3/22/20.
+//  Copyright © 2020 Erick Pac. All rights reserved.
+//
+
+import UIKit
+
+class TextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
