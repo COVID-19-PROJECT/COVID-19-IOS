b38330634cadb658420f3462629afe3afdde3e2f
Erick Pac <erickj.pac@gmail.com>
2020-03-22 22:37:49 -0600

    [added]: emergency contact cell

---
+++ gt-first/Source/Scenes/EmergencyContacts/Cells/EmergencyContactCell.swift
@@ -0,0 +1,26 @@
+//
+//  EmergencyContactCell.swift
+//  gt-first
+//
+//  Created by Erick Pac on 3/22/20.
+//  Copyright © 2020 Erick Pac. All rights reserved.
+//
+
+import UIKit
+
+class EmergencyContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        prepareUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func prepareUI() {
+        
+    }
+    
+}
