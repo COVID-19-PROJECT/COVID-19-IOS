3e3b0af754a22d9d15eed0bf314c5baeb3abc8e3
Erick Pac <erickj.pac@gmail.com>
2020-04-02 20:20:29 -0600

    [fixed]: page controller in onboarding view

--- gt-first/Source/Scenes/Onboarding/PageContainer/OnboardingPageContainerViewController.swift
+++ gt-first/Source/Scenes/Onboarding/PageContainer/OnboardingPageContainerViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class OnboardingPageContainerViewController: UIViewController {
     
-    var pageController: PageViewController?
+    @IBOutlet weak var buttonNextPage: UIButton!
+    
+    private var pageController: PageViewController?
+    private var pages: Int = 0
+    private var currentPage: Int = 0
     private let presenter = OnboardingPageContainerPresenter(apiManager: APIManager.shared)
     
     override func viewDidLoad() {
@@ -27,6 +31,7 @@ class OnboardingPageContainerViewController: UIViewController {
         super.prepare(for: segue, sender: sender)
         if let viewController = segue.destination as? PageViewController, segue.identifier == "onboardingSegue" {
             let controllers: [UIViewController] = presenter.prepareControllers()
+            pages = controllers.count
             viewController.orderedViewControllers = controllers
             viewController.pageChangeDelegate = self
             pageController = viewController
@@ -35,6 +40,28 @@ class OnboardingPageContainerViewController: UIViewController {
     
     private func prepareUI() {
         
+    }
+    
+    private func changePage(to index: Int) {
+        guard let pageControllerInstance = pageController else { return }
+        pageControllerInstance.changePage(to: index)
+    }
+    
+    private func updateNextAndFinishButton(indexPage: Int) {
+        buttonNextPage.setTitle(indexPage == (pages - 1) ? "FINALIZAR" : "CONTINUAR", for: .normal)
+    }
+    
+    @IBAction func buttonFinishTap(_ sender: Any) {
+        if currentPage == (pages - 1) {
+            let storyboard = K.Storyboards.Login.value
+            let viewController: UIViewController = storyboard.instantiateInitialViewController() ?? UIViewController()
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            appDelegate.setRootViewController(controller: viewController)
+        } else {
+            currentPage += 1
+            updateNextAndFinishButton(indexPage: currentPage)
+            changePage(to: currentPage)
+        }
     }
     
     deinit {
@@ -54,7 +81,8 @@ extension OnboardingPageContainerViewController: OnboardingPageContainerDelegate
 extension OnboardingPageContainerViewController: PageChangeProtocol {
     
     func pageChange(indexPage: Int) {
-        
+        currentPage = indexPage
+        updateNextAndFinishButton(indexPage: currentPage)
     }
     
 }
